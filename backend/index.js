8e1b24688bf9609c5ba8abee6b997e63e7d286c5
Ritika Rana <ritika.rana07@gmail.com>
2024-11-29 20:02:19 +0000

    Basic Setup for backend

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+// Load environment variables
+dotenv.config();
+
+// Connect to Database
+connectDB();
+
+const app = express();
+
+// Middleware for 
+app.use(express.json());
+
+// Placeholder route
+app.get('/', (req, res) => res.send('API is running...'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
